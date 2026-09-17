5e0d9400debcac746690335778c796027a2b719e
Cheibany <ciresall79@gmail.com>
2026-09-17 09:57:03 +0100

    chore: upgrade Angular dependencies to version 15.x and update TypeScript to 4.9.5
    - Updated Angular packages in package.json to version 15.x
    - Changed imports in app.module.ts to use legacy modules from Angular Material
    - Removed deprecated require context in test.ts
    - Updated TypeScript target to ES2022 in tsconfig.json and added useDefineForClassFields option

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,11 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
-import { MatCardModule } from '@angular/material/card';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { MatSelectModule } from '@angular/material/select';
-import { MatButtonModule } from '@angular/material/button';
+import { MatLegacyCardModule as MatCardModule } from '@angular/material/legacy-card';
+import { MatLegacyFormFieldModule as MatFormFieldModule } from '@angular/material/legacy-form-field';
+import { MatLegacyInputModule as MatInputModule } from '@angular/material/legacy-input';
+import { MatLegacySelectModule as MatSelectModule } from '@angular/material/legacy-select';
+import { MatLegacyButtonModule as MatButtonModule } from '@angular/material/legacy-button';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
